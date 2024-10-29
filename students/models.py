ef5d0ce928de452d97368a591baa7709423c1b9e
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-10-29 13:26:43 +0300

    commit 2

--- students/models.py
+++ students/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Student(models.Model):
     FIRST_YEAR = 'first'
@@ -17,7 +22,7 @@ class Student(models.Model):
     first_name = models.CharField(max_length=150, verbose_name='Имя')
     last_name = models.CharField(max_length=150, verbose_name='Фамилия')
     year = models.CharField(max_length=6, choices=YEAR_IN_SCHOOL_CHOICES, default=FIRST_YEAR, verbose_name='Курс')
-
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='students')
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
